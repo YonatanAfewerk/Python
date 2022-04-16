11f05dfc127bbdd60cddac692302c0e045e2b444
yonatan <yonatanafewerk@gmail.com>
2022-04-16 16:46:01 +0300

    Sat,April16,2022

---
+++ Python/Class/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+elif((num1 == num3) < num2):
+    print(num3, num1, num2)
---
+++ Python/Class/try2.py
@@ -0,0 +1,8 @@
+maximum = int(input(" Please Enter the Maximum Value : "))
+total = 0
+
+for number in range(1, maximum+1):
+    if(number % 2 == 0):
+        total = total + number
+
+print("The Sum of Even Numbers from 1 to {0} = {1}".format(number, total))
