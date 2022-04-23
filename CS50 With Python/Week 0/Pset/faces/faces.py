8e04b4ea1d7bd4ba17198428e7715ee9ba30e3bf
yonatan <yonatanafewerk@gmail.com>
2022-04-23 10:43:44 +0300

    Sat,23,2022, Done cs50week0

--- CS50 With Python/Week 0/Pset/faces/faces.py
+++ CS50 With Python/Week 0/Pset/faces/faces.py
@@ -1,9 +1,12 @@
+ # import emoji to get the emoji faces 
 import emoji
 
 w = input("")
 
+    # To make sure there are no spaces in the front and the back of the input we use strip()
 word = w.strip()
 
+    # checking the input agains out condition we will out put the right message to the user and
 if (word == "Hello :)"):
     print("Hello", "\N{slightly smiling face}")
 elif (word == "Goodbye :("):
