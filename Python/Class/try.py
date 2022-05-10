f02a1fe5bea1292a9cb90be13e659e04222534be
yonatan <yonatanafewerk@gmail.com>
2022-05-10 18:17:36 +0300

    Tue,May10,2022 Cso50 lecture4 done on to the psets

--- Python/Class/try.py
+++ Python/Class/try.py
@@ -1,1 +1,14 @@
- 
+import requests
+from sys import argv, exit 
+import json
+
+if argv[1] != 2:
+    exit(1)
+
+response = requests.get(
+    f"https://itunes.apple.com/search?entity=song&limit=1&term={sys.argv[1]}")
+
+o = response.json()
+
+for result in o["results"]:
+    print("Artist Name: ", result["artistName"])
--- Python/Class/try4.py
+++ Python/Class/try4.py
@@ -1,8 +1,6 @@
 import cowsay 
 import sys
+from module import hello, goodbye
 
 if len(sys.argv) == 2:
-    cowsay.trex("Hello, " + sys.argv[1])
-
-
-
+    goodbye(sys.argv[1])
